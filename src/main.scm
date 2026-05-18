7fd2d9419f3f42543f9fb1a581e8baafae203e12
barbeariaconfallony <barbeariaconfallony62@gmail.com>
2026-05-17 23:20:38 -0300

    Auto-sync design/code

--- src/main.scm
+++ src/main.scm
@@ -2,64 +2,24 @@
   "Properties": {
     "$Type": "Form",
     "$Name": "main",
-    "Title": "Autenticação",
+    "Title": "Apresentação",
     "BackgroundColor": "&HFFFFFFFF",
     "TitleVisible": "False",
     "$Components": [
       {
         "$Type": "Label",
         "$Name": "Label1",
-        "Text": "Login",
-        "FontSize": 24,
+        "Text": "Bem-vindo!",
+        "FontSize": 36,
         "FontBold": true,
         "TextColor": "&HFF000000",
         "Width": -1,
         "Height": -1
       },
       {
-        "$Type": "TextBox",
-        "$Name": "TextBox1",
-        "Hint": "Usuário",
-        "Width": -2,
-        "Height": -1
-      },
-      {
-        "$Type": "TextBox",
-        "$Name": "TextBox2",
-        "Hint": "Senha",
-        "Password": true,
-        "Width": -2,
-        "Height": -1
-      },
-      {
         "$Type": "Button",
         "$Name": "Button1",
         "Text": "Entrar",
-        "Width": -2,
-        "Height": -1,
-        "BackgroundColor": "&HFF4CAF50",
-        "TextColor": "&HFFFFFFFF"
-      },
-      {
-        "$Type": "CheckBox",
-        "$Name": "CheckBox1",
-        "Text": "Lembre-me",
-        "Width": -1,
-        "Height": -1
-      },
-      {
-        "$Type": "Label",
-        "$Name": "Label2",
-        "Text": "Esqueceu a senha?",
-        "FontSize": 14,
-        "TextColor": "&HFF000000",
-        "Width": -1,
-        "Height": -1
-      },
-      {
-        "$Type": "Button",
-        "$Name": "Button2",
-        "Text": "Cadastrar",
         "Width": -2,
         "Height": -1,
         "BackgroundColor": "&HFF4CAF50",
