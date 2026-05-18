b455727f12a9bc49131fcacd801b43e551d2928f
barbeariaconfallony <barbeariaconfallony62@gmail.com>
2026-05-17 23:17:24 -0300

    Create screen login

---
+++ src/login.scm
@@ -0,0 +1,11 @@
+{
+  "Properties": {
+    "$Type": "Form",
+    "$Name": "login",
+    "Title": "login",
+    "BackgroundColor": "&HFFFFFFFF",
+    "AlignHorizontal": "1",
+    "AlignVertical": "1",
+    "$Components": []
+  }
+}
