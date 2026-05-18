9ffa5a8ba8006777563f4272987b36a6de3c1184
barbeariaconfallony <barbeariaconfallony62@gmail.com>
2026-05-17 22:19:37 -0300

    Create screen main

---
+++ src/main.scm
@@ -0,0 +1,11 @@
+{
+  "Properties": {
+    "$Type": "Form",
+    "$Name": "main",
+    "Title": "main",
+    "BackgroundColor": "&HFFFFFFFF",
+    "AlignHorizontal": "1",
+    "AlignVertical": "1",
+    "$Components": []
+  }
+}
