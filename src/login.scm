5d4ac0ba1c31de4ae36fe98d482b33bf38e73d99
barbeariaconfallony <barbeariaconfallony62@gmail.com>
2026-05-17 23:39:47 -0300

    Auto-sync design/code

--- src/login.scm
+++ src/login.scm
@@ -2,10 +2,71 @@
   "Properties": {
     "$Type": "Form",
     "$Name": "login",
-    "Title": "login",
+    "Title": "Login",
     "BackgroundColor": "&HFFFFFFFF",
-    "AlignHorizontal": "1",
-    "AlignVertical": "1",
-    "$Components": []
+    "$Components": [
+      {
+        "$Type": "VerticalArrangement",
+        "$Name": "Container",
+        "Width": -2,
+        "Height": -2,
+        "AlignHorizontal": 3,
+        "AlignVertical": 2,
+        "$Components": [
+          {
+            "$Type": "Label",
+            "$Name": "LblTitulo",
+            "Text": "Bem-vindo!",
+            "FontSize": 24,
+            "FontBold": true
+          },
+          {
+            "$Type": "Space",
+            "$Name": "Space1",
+            "Height": 20
+          },
+          {
+            "$Type": "TextBox",
+            "$Name": "TxtEmail",
+            "Hint": "Email",
+            "Width": -2
+          },
+          {
+            "$Type": "Space",
+            "$Name": "Space2",
+            "Height": 10
+          },
+          {
+            "$Type": "PasswordTextBox",
+            "$Name": "TxtSenha",
+            "Hint": "Senha",
+            "Width": -2
+          },
+          {
+            "$Type": "Space",
+            "$Name": "Space3",
+            "Height": 20
+          },
+          {
+            "$Type": "Button",
+            "$Name": "BtnEntrar",
+            "Text": "Entrar",
+            "Width": -2
+          },
+          {
+            "$Type": "Button",
+            "$Name": "BtnEsqueciSenha",
+            "Text": "Esqueci minha senha",
+            "Width": -2
+          },
+          {
+            "$Type": "Button",
+            "$Name": "BtnCadastro",
+            "Text": "Cadastrar",
+            "Width": -2
+          }
+        ]
+      }
+    ]
   }
 }
